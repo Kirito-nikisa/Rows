9c573438bf577591b74dc94162d8faf51e8d6238
Kirito-nikisa <nikosa090@gmail.com>
2024-06-19 19:18:39 +0300

    Added lab ex2

--- Rows/main.cpp
+++ Rows/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
-#include <string>
+#include <cctype>
+
 using namespace std;
 
 int main() {
-  
-    
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    int letter_count = 0;
+    int digit_count = 0;
+    int other_count = 0;
+
+    for (char c : input) {
+        if (isalpha(c)) {
+            letter_count++;
+        }
+        else if (isdigit(c)) {
+            digit_count++;
+        }
+        else {
+            other_count++;
+        }
+    }
+
+    cout << "Letters: " << letter_count << endl;
+    cout << "Digits: " << digit_count << endl;
+    cout << "Other characters: " << other_count << endl;
 
     return 0;
 }
