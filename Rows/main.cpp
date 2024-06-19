1f1fd558e296c942296241a32fc62e5378689c9a
Kirito-nikisa <nikosa090@gmail.com>
2024-06-19 19:35:26 +0300

    Added lab ex3

--- Rows/main.cpp
+++ Rows/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+
 using namespace std;
 
 int main() {
-  
-    
+    string sentence;
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);
+
+    stringstream ss(sentence);
+    string word;
+    int word_count = 0;
+
+    while (ss >> word) {
+        word_count++;
+    }
+
+    cout << "Number of words in the sentence: " << word_count << endl;
 
     return 0;
 }
