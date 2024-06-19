d37e8d35db8978758a7d7f7ae03b276df5939a3b
Kirito-nikisa <nikosa090@gmail.com>
2024-06-19 20:07:34 +0300

    Added lab ex4

--- Rows/main.cpp
+++ Rows/main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <string>
+#include <cctype> 
+
 using namespace std;
 
-int main() {
-  
+bool isPalindrome(const string& str) {
+    int left = 0;
+    int right = str.length() - 1;
+
+    while (left < right) {
+      
+        while (!isalnum(str[left]) && left < right) {
+            left++;
+        }
+        while (!isalnum(str[right]) && left < right) {
+            right--;
+        }
     
+        if (tolower(str[left]) != tolower(str[right])) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    if (isPalindrome(input)) {
+        cout << "The string is a palindrome." << endl;
+    }
+    else {
+        cout << "The string is not a palindrome." << endl;
+    }
 
     return 0;
 }
