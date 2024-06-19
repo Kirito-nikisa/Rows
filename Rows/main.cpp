fdcc09ef576e895aab2ec6a858402f03593c94ba
Kirito-nikisa <nikosa090@gmail.com>
2024-06-19 19:16:52 +0300

    ADDED lab ex1

--- Rows/main.cpp
+++ Rows/main.cpp
@@ -3,8 +3,16 @@
 using namespace std;
 
 int main() {
-  
-    
+    string input;
+    getline(cin, input);
+
+    for (char& c : input) {
+        if (c == ' ') {
+            c = '\t';
+        }
+    }
+
+    cout << "String after replacement: " << input << endl;
 
     return 0;
 }
