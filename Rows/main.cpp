dc0edb13acd9d81bac5fd18364758387ae057e1a
Kirito-nikisa <nikosa090@gmail.com>
2024-06-29 17:31:47 +0300

    Added hom ex4

--- Rows/main.cpp
+++ Rows/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+string replaceDotsWithExclamations(string str) {
+    for (char& ch : str) {
+        if (ch == '.') {
+            ch = '!';
+        }
+    }
+    return str;
+}
+
 int main() {
-  
-    
+    string input;
+
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    string result = replaceDotsWithExclamations(input);
+    cout << "Result: " << result << endl;
 
     return 0;
 }
