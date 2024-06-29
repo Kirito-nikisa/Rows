8191d6eb88c3dfa1dcc3ce64d3084a66924d70f6
Kirito-nikisa <nikosa090@gmail.com>
2024-06-29 17:29:55 +0300

    Added hom ex2

--- Rows/main.cpp
+++ Rows/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+string removeAllOccurrences(string str, char ch) {
+    str.erase(remove(str.begin(), str.end(), ch), str.end());
+    return str;
+}
+
 int main() {
-  
-    
+    string input;
+    char ch;
+
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    cout << "Enter the character to remove: ";
+    cin >> ch;
+
+    string result = removeAllOccurrences(input, ch);
+    cout << "Result: " << result << endl;
 
     return 0;
 }
