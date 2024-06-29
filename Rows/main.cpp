b4122ebea463ba6bc8047daab8ff90026b395df3
Kirito-nikisa <nikosa090@gmail.com>
2024-06-29 17:27:50 +0300

    Added hom ex1

--- Rows/main.cpp
+++ Rows/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+string removeCharacter(string str, int index) {
+    if (index < 0 || index >= str.length()) {
+        cerr << "Index out of bounds!" << endl;
+        return str;
+    }
+    str.erase(index, 1);
+    return str;
+}
+
 int main() {
-  
-    
+    string input;
+    int index;
+
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    cout << "Enter the index of the character to remove: ";
+    cin >> index;
+
+    string result = removeCharacter(input, index);
+    cout << "Result: " << result << endl;
 
     return 0;
 }
