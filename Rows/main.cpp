bfc7b07db34d3bc4cfe45233f3c784b61c033fce
Kirito-nikisa <nikosa090@gmail.com>
2024-06-29 17:30:50 +0300

    Added hom ex3

--- Rows/main.cpp
+++ Rows/main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+string insertCharacter(string str, char ch, int index) {
+    if (index < 0 || index > str.length()) {
+        cerr << "Index out of bounds!" << endl;
+        return str;
+    }
+    str.insert(str.begin() + index, ch);
+    return str;
+}
+
 int main() {
-  
-    
+    string input;
+    char ch;
+    int index;
+
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    cout << "Enter the character to insert: ";
+    cin >> ch;
+
+    cout << "Enter the index at which to insert the character: ";
+    cin >> index;
+
+    string result = insertCharacter(input, ch, index);
+    cout << "Result: " << result << endl;
 
     return 0;
 }
