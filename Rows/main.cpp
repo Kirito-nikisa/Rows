71db2c1c290678656b7920a0e245638b358bd475
Kirito-nikisa <nikosa090@gmail.com>
2024-06-29 17:32:38 +0300

    Added hom 5

--- Rows/main.cpp
+++ Rows/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+int countOccurrences(const string& str, char ch) {
+    int count = 0;
+    for (char currentChar : str) {
+        if (currentChar == ch) {
+            count++;
+        }
+    }
+    return count;
+}
+
 int main() {
-  
-    
+    string input;
+    char ch;
+
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    cout << "Enter the character to count: ";
+    cin >> ch;
+
+    int count = countOccurrences(input, ch);
+    cout << "The character '" << ch << "' appears " << count << " times in the string." << endl;
 
     return 0;
 }
